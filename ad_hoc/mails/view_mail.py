63c1b23846c411210d28abd162aaa7c4cfd87b26
shenglin1991 <linsheng9090@hotmail.com>
2016-07-28 15:08:03 +0200

    use read interface to view mails

--- ad_hoc/mails/view_mail.py
+++ ad_hoc/mails/view_mail.py
@@ -10,69 +10,57 @@ import webbrowser
 import click
 
 from storages.message import redis_conn
-from storages.mongo_db import mongo_conn, mongo_reader
 
 
-def get_body(db, body):
+def get_body(body):
     body_value = ''
-    # for part in body['value']:
-    if isinstance(body.get('value'), list):
-        for part in body.get('value'):
-            part_body = mongo_reader(db, collection=part.get('collection', 'multipart'),
-                                     filtre={'_id': part.get('address')})
-            body_value += (get_body(db, part_body) if isinstance(part_body.get('value'), list)
-                           else part_body.get('value'))
+    if isinstance(body, list):
+        for part in body:
+            body_value += part.get('value')
     else:
         body_value += body.get('value')
     return body_value
 
 
-def view(db, mail_table, mail_id, fields):
+def view(mail_storage, mail_table, mail_id, fields):
     mail = None
     redis = redis_conn()
     result_subscription = redis.pubsub(ignore_subscribe_messages=True)
     result_subscription.subscribe('read_result')
 
     redis.publish('read', json.dumps({
-        'storage': db,
+        'storage': mail_storage,
         'collection': mail_table,
         'filtre': {
             '_id': mail_id
-        }
+        },
+        'projection': fields
     }))
 
     for msg in result_subscription.listen():
         mail = json.loads(msg['data']).get('result')
         break
 
-    # mail = mongo_reader(db, collection=mail_table, filtre={'_id': ObjectId(mail_id)})
-
     if not mail:
         raise ValueError('mail not found')
     # open body in a web browser
-    if 'body' in fields:
+    if 'body' in fields.split(','):
         with open('body.html', 'w') as fp:
-            body = mongo_reader(db, collection='body', filtre={'_id': mail['body'].get('address')})
-            fp.write(get_body(db, body).decode('utf-8').encode('latin-1'))
+            body = mail.get('body')
+            fp.write(get_body(body).encode('latin-1'))
         webbrowser.open_new_tab('body.html')
-        fields.remove('body')
-
-    for key in fields:
-        print key + ': ' + mongo_reader(db, collection=key, filtre={'_id': mail.get(key).get('address')}).get('value')
+    for key, value in mail.iteritems():
+        if key != 'body':
+            print key + ': ' + value
 
 
 @click.command()
 @click.option('--id', default=None)
 @click.option('--fields', default='Subject,From,To')
-def main(id, fields, conf=os.environ, default_mail_storage=None):
-    if not default_mail_storage:
-        default_mail_storage = mongo_conn({
-            '__DB_ADDR__': conf.get('__DB_ADDR__', 'localhost:27027')
-        })
-
+def main(id, fields, conf=os.environ):
     print 'Start visualizing email'
     start = time.time()
-    view(default_mail_storage, conf.get('mail_table', 'parsed_mails'), id, fields.split(','))
+    view('mongo_db2', conf.get('mail_table', 'parsed_mails'), id, fields)
     print 'visual time: {}ms'.format(int((time.time() - start) * 1000))
 
 
